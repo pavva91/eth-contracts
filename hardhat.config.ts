514c02743cf37eeb6ab412f5a7491f5697077492
pavva91 <valerio.mattioli@protonmail.com>
2023-06-28 18:27:34 +0200

    Use Sepolia (Infura) test network (secrets on .env)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,17 @@
-import "@nomiclabs/hardhat-waffle";
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+import "hardhat-gas-reporter";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
   networks: {
     hardhat: {
-      chainId: 1337
-    }
-  }
+      chainId: 1337,
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.SEPOLIA_SECRET_KEY_ACCOUNT}`],
+    },
+  },
 };
