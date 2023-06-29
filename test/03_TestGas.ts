21802f88379b8b51eb90919f281fb5dd5ae79c84
pavva91 <valerio.mattioli@protonmail.com>
2023-06-29 11:39:24 +0200

    Cleaned tests on gas consumption

--- test/03_TestGas.ts
+++ test/03_TestGas.ts
@@ -2,8 +2,8 @@ import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
-describe("TestGas", function() {
-  it("Test", async function() {
+describe("Test Gas Consumption of opcodes", function() {
+  it("Test Add operation increasing", async function() {
     // 1. setup
     // 2. deploy out contract
     // 3. call our functions to test
@@ -17,6 +17,12 @@ describe("TestGas", function() {
       await gas.test1();
       await gas.test2();
       await gas.test3();
+    }
+  });
+
+  it("Test function operation", async () => {
+    const gas = await ethers.deployContract("TestGas");
+    for (let i = 0; i < 10; i++) {
       await gas.test4();
       await gas.test5();
     }
