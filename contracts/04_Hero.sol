e2e17a48414c9ab51f4e5c1f5855d13fa28a2553
pavva91 <valerio.mattioli@protonmail.com>
2023-06-29 16:34:38 +0200

    Add storing and reading operations (bitwise operations to optimize storing)

--- contracts/04_Hero.sol
+++ contracts/04_Hero.sol
@@ -10,9 +10,9 @@ pragma solidity ^0.8.10;
 //     - 0.05 ETH per hero
 // - I should be able to get mmy heroes I have generated
 // - Heroes should be stored on the chain
-// - Stats are: strength, health, intellect, magic, dexterity
+// - Stats are: strength, health, dexterity, intellect, magic
 // - Stats are randomly generated
-//     - A scale of 1-18
+//     - A scale of 1-18 (18 can be represented on binary as 0b10010, so only 5 bits are needed to represent each stat)
 //     - The stats are randomly picked and their amplitude is randomly dtermined according to the following:
 //         - Stat 1 can max at 18
 //         - Stat 2 can max at 17
@@ -22,17 +22,20 @@ pragma solidity ^0.8.10;
 
 contract Hero {
     enum Class {
-        Mage,
-        Healer,
-        Barbarian
+        Mage, // 0, 0b00
+        Healer, // 1, 0b01
+        Barbarian // 2, 0b11
     }
 
     // Takes addresses and produces out arrays
     mapping(address => uint[]) addressToHeroes;
 
-    // FIX: for NFTs use chainlinkVRF to generate random numbers
+    // FIX: for NFTs use chainlinkVRF to generate random numbers (https://vrf.chain.link/)
     function generateRandom() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+        return
+            uint(
+                keccak256(abi.encodePacked(block.difficulty, block.timestamp))
+            );
     }
 
     // Provider is enough
@@ -40,8 +43,54 @@ contract Hero {
         return addressToHeroes[msg.sender];
     }
 
+    function getStrength(uint hero) public view returns (uint[] memory) {
+        return (hero >> 2) & 0x1F;
+        // with hero >> 2 we shifted the class off and now strenght represents the first 5 bits
+        // Select out only the first 5 bits: 0x1F = 0b11111
+    }
+
+    function getHealth(uint hero) public view returns (uint[] memory) {
+        return (hero >> 7) & 0x1F;
+    }
+
+    function getDexterity(uint hero) public view returns (uint[] memory) {
+        return (hero >> 12) & 0x1F;
+    }
+
+    function getIntellect(uint hero) public view returns (uint[] memory) {
+        return (hero >> 17) & 0x1F;
+    }
+
+    function getMagic(uint hero) public view returns (uint[] memory) {
+        return (hero >> 22) & 0x1F;
+    }
+
     // Signer is needed
     function createHero(Class class) public payable {
         require(msg.value >= 0.05 ether, "Please send more money");
+
+        // Stats are: strength, health, dexterity, intellect, magic
+        uint[] stats = new uint[](5);
+        stats[0] = 2; // represent strenght (first 2 bits are the enum 0b00 is Mage, 0b01 is Healer, 0b11 is Barbarian)
+        stats[1] = 7; // represent health
+        stats[2] = 12; // represent dexterity
+        stats[3] = 17; // represent intellect
+        stats[4] = 22; // represent magic
+
+        uint len = 5;
+        uint hero = uint(class);
+
+        do {
+            uint pos = generateRandom() % len;
+            uint value = (generateRandom() % (13 + len)) + 1;
+
+            hero |= value << stats[pos];
+
+            len--;
+            stats[pos] = stats[len];
+        } while (len > 0);
+
+        // Insert created hero into the addressToHeroes mapping
+        addressToHeroes[msg.sender].push(hero);
     }
 }
