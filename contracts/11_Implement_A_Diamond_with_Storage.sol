80bd363f631002d943e72d4d6d1a295ddbfed1da
pavva91 <valerio.mattioli@protonmail.com>
2023-06-30 20:28:29 +0200

    Implement diamond with storage

---
+++ contracts/11_Implement_A_Diamond_with_Storage.sol
@@ -0,0 +1,42 @@
+// TODO: git clone https://github.com/mudgen/diamond-3-hardhat
+// cd diamond-3-hardhat
+// put this file inside contracts
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// NOTE: Interface of A
+interface IA {
+    function getA() external pure returns (uint256);
+    function setA(uint256 _a) external; 
+}
+
+// NOTE: Storage of the diamond (of A)
+struct AppStorage {
+    uint256 a;
+}
+
+// NOTE: workaround to have the storage not override
+library ALib {
+    bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("my-storage-location");
+
+    function get() internal pure returns (AppStorage storage s) {
+        bytes32 k = DIAMOND_STORAGE_POSITION;
+        assembly {
+            s.slot := k
+        }
+    }
+}
+
+// NOTE: Actual contract
+contract A {
+    AppStorage s;
+
+    function setA(uint256 _a) public {
+        s.a = _a;
+    }
+
+    function getA() public view returns (uint256) {
+        return s.a;
+    }
+}
