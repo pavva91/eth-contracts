ccbd8bdd008cfa8add1f6dd9266301d1e09be135
pavva91 <valerio.mattioli@protonmail.com>
2023-06-30 13:30:35 +0200

    Reordered call, retrieve fallback deploy script

--- scripts/03-deploy-fallback.ts
+++ scripts/03-deploy-fallback.ts
@@ -1,34 +1,18 @@
 import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
-async function deploy(name, ...args) {
-  const Fallback = await ethers.getContractFactory(name);
-  const fallback = await Fallback.deploy(...args);
-  await fallback.deployed();
+async function deploy() {
+  const fallback = await ethers.deployContract("Fallback");
 
   console.log(fallback.address);
 
   return fallback;
 }
 
-async function fallback() {
-  const a = await deploy("A");
-  const b = await deploy("B", a.address);
-
-  console.log("A", await a.getA());
-  console.log("B", await b.getB());
-  console.log("----------");
-
-  await a.setA(42);
-  console.log("A", await a.getA());
-  console.log("B", await b.getB());
-  console.log("----------");
-
-  await b.setB(60);
-  console.log("A", await a.getA());
-  console.log("B", await b.getB());
-  console.log("----------");
-
+async function fallback(fallback) {
+  const f = await ethers.getContractAt("IFallback", fallback.address);
+  await f.count();
 }
 
-fallback();
+deploy().then(fallback);
+
